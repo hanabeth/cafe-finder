53fc5d743ad841b472c9831379480b6b20ee918e
Hana Johnson <zencodecat@gmail.com>
2021-10-30 20:44:16 -0600

    add more styling and create banner component

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
+        <title>Find Cafes!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
